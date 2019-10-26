e18d9fa19cd55372876002835ef76cdcf7b57ce9
sungyeon <sungyeonjoo@naver.com>
2019-10-26 13:17:27 +0900

    organized directory

---
+++ front-end/wearhouse/src/Containers/EditOutfit/EditOutfit.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class EditOutfit extends Component {
+    render() {
+        return <div className="EditOutfit"></div>;
+    }
+}
+export default EditOutfit;
