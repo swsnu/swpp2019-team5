9cb7d676affae763182dabfd6d8a174326c0a7b9
ttoru96 <ttoru96jk@gmail.com>
2019-10-26 14:35:19 +0900

    Format code according to prettier configuration

--- front-end/wearhouse/src/store/reducers/login.js
+++ front-end/wearhouse/src/store/reducers/login.js
@@ -1,7 +1,7 @@
 import * as actionTypes from "../actions/actionTypes";
 const initialState = {
     isLoggedIn: false,
-    userID: null
+    userID: null,
 };
 
 const reducer = (state = initialState, action) => {};
