9ffd267558893ca4f07f40273e256305e1aed7bf
sungyeon <sungyeonjoo@naver.com>
2019-10-28 16:23:57 +0900

    Specified tasks in comments

--- front-end/wearhouse/src/Containers/CreateOutfit/CreateOutfit.js
+++ front-end/wearhouse/src/Containers/CreateOutfit/CreateOutfit.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
+//outfit-image : image
+//edit - item : EditItem button- mode controller
+//add - item : button - add new item
+//confirm-create-button : load data to database
 
 class CreateOutfit extends Component {
     render() {
         return <div className="CreateOutfit"></div>;
     }
 }
-export default CreateOutfit;
+
+export default connect(CreateOutfit);
