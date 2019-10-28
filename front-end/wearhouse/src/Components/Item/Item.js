e49a0cc25b5fd2d8e64bd5d263c5f8ea1c29020c
sungyeon <sungyeonjoo@naver.com>
2019-10-29 00:55:02 +0900

    Implement addTag function

--- front-end/wearhouse/src/Components/Item/Item.js
+++ front-end/wearhouse/src/Components/Item/Item.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import Tag from "../Tag/Tag";
 //props : item, applyEdit(edit_item), delete
-//check whether input tag is existing in database (maybe Sprint4)
+// further task #1 check whether input tag is existing in database (maybe Sprint4)
 
 class Item extends Component {
     state = {
         category: this.props.item.category,
         tags: this.props.item.tags,
-        todo: "Add tag",
+        todo: "Add tag", //the mode where user enters after clicing button
     };
     handleCategoryChange(event) {
         this.setState({ category: event.target.value });
@@ -20,6 +20,15 @@ class Item extends Component {
     //Delete Tag
     onDeleteTag(tag) {}
 
+    //add Tag
+    addTag(e) {
+        if (e.keyCode == 13) {
+            tags = this.state.tags;
+            tags = tags.concat(e.value);
+            this.setState({ tags: tags });
+        }
+    }
+
     //convert the todo ("Add tag" or "Confirm")
     changeMode = () => {
         if (this.state.todo === "Add tag") this.setState({ todo: Confirm });
@@ -29,7 +38,16 @@ class Item extends Component {
         let tags = this.props.item.tags.map(tag => {
             return <Tag tag={tag} delete={() => this.onDeleteTag(tag)} />;
         });
-        let tag_input = <input type="text" placeholder="Enter tag"></input>;
+        let tag_input = null;
+        if (this.state.todo === "Confirm")
+            tag_input = (
+                <input
+                    type="text"
+                    placeholder="Enter tag"
+                    //onChange = further task #1
+                    onKeyDown={e => this.addTag(e)}
+                ></input>
+            );
         return (
             <div className="Item">
                 <select
