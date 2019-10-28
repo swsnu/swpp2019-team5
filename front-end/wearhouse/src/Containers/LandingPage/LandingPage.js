5340673146155959b0f4d28141d5f72f948d2100
sungyeon <sungyeonjoo@naver.com>
2019-10-28 20:32:42 +0900

    Resolved code review comment #4

--- front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
+++ front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 
-class Main extends Component {
+class LandingPage extends Component {
     render() {
         return <div className="Main"></div>;
     }
 }
-export default Main;
+export default LandingPage;
