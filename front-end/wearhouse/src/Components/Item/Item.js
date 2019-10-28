a22635974e580821af54650d1a69e150e0f4e3aa
sungyeon <sungyeonjoo@naver.com>
2019-10-28 23:49:36 +0900

    Implement handleTagChange

--- front-end/wearhouse/src/Components/Item/Item.js
+++ front-end/wearhouse/src/Components/Item/Item.js
@@ -1,20 +1,34 @@
 import React, { Component } from "react";
 //props : item, applyEdit(edit_item), delete
+//check whether input tag is existing in database
+
 class Item extends Component {
-    state = { category: this.props.item.category, tags: this.props.item.tags };
-    handleChange(event) {
+    state = { category: this.props.item.category, tags: this.props.item.tags, edit_mode:false };
+    handleCategoryChange(event) {
         this.setState({ category: event.target.value });
         this.props.applyEdit({
             category: this.state.category,
             tags: this.state.item.tags,
         });
     }
+
+
+
+    handleTagChange(event) {
+        tags = this.trimTagInputs(event.target.value)
+        this.setState({tags : tags});
+        this.props.applyEdit({
+            category: this.state.category,
+            tags: this.state.item.tags,
+        });
+    }
     render() {
+        let tag_string = this.state.tags.map(tag => )
         return (
             <div className="Item">
                 <select
                     value={this.state.category}
-                    onChange={this.handleChange}
+                    onChange={this.handleCategoryChange}
                 >
                     <option value="outer">Outer</option>
                     <option value="top">Top</option>
