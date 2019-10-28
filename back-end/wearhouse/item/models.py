be453d24aabb1f6acbb6c69e25c39d251bd8c535
Kwaktaewon <tykwak94@gmail.com>
2019-10-28 17:08:53 +0900

    solving circular import problem

--- back-end/wearhouse/item/models.py
+++ back-end/wearhouse/item/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from tag.models import Tag
-from outfit.models import Outfit
 # Create your models here.
 
 class Item(models.Model):
     tags = models.ManyToManyField(Tag, related_name= 'tag_list')
     user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'items_owned_by_user')
-    outfit = models.ManyToManyField(Outfit, on_delete=models.CASCADE, related_name= 'items_included_in_outfit')
--- back-end/wearhouse/tag/models.py
+++ back-end/wearhouse/tag/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-from item.models import Item
 # Create your models here.
+
 class Tag(models.Model):
     name = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'tags_owned_by_user')
-    item = models.ManyToManyField(Item, on_delete = models.CASCADE, related_name= 'tags_included_in_item')
