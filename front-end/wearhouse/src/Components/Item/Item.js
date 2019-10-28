d0e5a3e2ecdc9f0ed15b6d173f871978984fe51a
sungyeon <sungyeonjoo@naver.com>
2019-10-29 00:08:43 +0900

    Handle tag changes using Tag components (in progress)

--- front-end/wearhouse/src/Components/Item/Item.js
+++ front-end/wearhouse/src/Components/Item/Item.js
@@ -3,7 +3,11 @@ import React, { Component } from "react";
 //check whether input tag is existing in database
 
 class Item extends Component {
-    state = { category: this.props.item.category, tags: this.props.item.tags, edit_mode:false };
+    state = {
+        category: this.props.item.category,
+        tags: this.props.item.tags,
+        edit_mode: false,
+    };
     handleCategoryChange(event) {
         this.setState({ category: event.target.value });
         this.props.applyEdit({
@@ -12,25 +16,31 @@ class Item extends Component {
         });
     }
 
-    
     //conver #black #shirt form to ["black,"shirt"]
     //gurantee that input form is correct
-    trimTagInputs(tags){
-        tag_strings = tags.split(" ")
-        tags = tag_strings.map(tag => tag.substring(1))
+    trimTagInputs(tags) {
+        tag_strings = tags.split(" ");
+        tags = tag_strings.map(tag => tag.substring(1));
         return tags;
     }
 
+    /*check whether input form is correct
+    input parameter ex) "#black #shirt"
+    */
+    checkInputForm(tags) {}
+
     handleTagChange(event) {
-        tags = this.trimTagInputs(event.target.value)
-        this.setState({tags : tags});
-        this.props.applyEdit({
-            category: this.state.category,
-            tags: this.state.item.tags,
-        });
+        validCheck = checkInputForm(event.target.value); //check whether input form is correct
+        if (validCheck) {
+            tags = this.trimTagInputs(event.target.value);
+            this.setState({ tags: tags });
+            this.props.applyEdit({
+                category: this.state.category,
+                tags: this.state.item.tags,
+            });
+        }
     }
     render() {
-        let tag_string = this.state.tags.map(tag => )
         return (
             <div className="Item">
                 <select
---
+++ front-end/wearhouse/src/Components/Tag/Tag.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class Tag extends Component {
+    render() {
+        return <div className="Tag"></div>;
+    }
+}
+export default Tag;
