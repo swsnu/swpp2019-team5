ad6ca07088b0497b0e0febc8fa46a7bcdc332cff
ttoru96 <ttoru96jk@gmail.com>
2019-10-29 15:33:23 +0900

    Pop up UploadImage Container when add-button has been clicked

--- front-end/wearhouse/src/Components/AddOutfit/AddOutfit.js
+++ front-end/wearhouse/src/Components/AddOutfit/AddOutfit.js
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
-
+import UploadImage from "../../Containers/Browse/UploadImage/UploadImage";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import "./AddOutfit.scss";
 
 class AddOutfit extends Component {
-    onClickButton = () => {
-        /*To Soyeong.. please implement here. as soon as user clicks Add outfit button 
-        uploading photo should pop up*/
+    state = {
+        showPopUp: false,
     };
+
+    onClickButton = () => {
+        this.setState({ ...this.state, showPopUp: true });
+    };
+
     render() {
+        let pop_up = "";
+        if (this.state.showPopUp) {
+            pop_up = <UploadImage />;
+        }
         return (
             <div id="add-outfit">
+                {pop_up}
                 <button id="add-outfit-button" onClick={this.onClickButton}>
                     <FontAwesomeIcon icon={faPlus} id="add-outfit-icon" />
                     <div id="add-outfit-text">Add New</div>
--- front-end/wearhouse/src/Containers/Browse/UploadImage/UploadImage.js
+++ front-end/wearhouse/src/Containers/Browse/UploadImage/UploadImage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import "./UploadImage.scss";
 
 class UploadImage extends React.Component {
     render() {
-        return <div id="upload-image-pop-up"></div>;
+        console.log("UploadImage");
+        return (
+            <div id="pop-up-container">
+                <h1>Hello World</h1>
+            </div>
+        );
     }
 }
+
+export default UploadImage;
