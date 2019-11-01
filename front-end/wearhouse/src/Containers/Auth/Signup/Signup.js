960e3922ad69eceed7919508ba5a89ed966528ee
Heather Kim <haesoo1108@gmail.com>
2019-11-01 23:57:26 +0900

    :sparkles: Signup component interface prototype

--- front-end/wearhouse/src/Containers/Auth/Signup/Signup.js
+++ front-end/wearhouse/src/Containers/Auth/Signup/Signup.js
@@ -2,25 +2,57 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actionCreators from "../../../store/actions/index";
 
+import "./Signup.scss";
+
 class Signup extends Component {
+    state = { password: "", passwordConfirm: "" };
     onSignUp = () => {
         //TODO: implement axios call
     };
+
+    // onPasswordConfirm = () => {
+    //     this.setState({
+    //         passwordMatch: this.state.password === this.state.passwordConfirm,
+    //     });
+    // };
+
     render() {
+        let visible = this.state.password === this.state.passwordConfirm;
+        console.log(this.state);
         return (
             <div id="signup">
-                <form onSubmit={this.onSignUp()}>
+                <form id="signup-form" onSubmit={this.onSignUp()}>
                     <label>E-mail</label>
                     <input type="text" name="email" id="email-input"></input>
                     <label>Password</label>
-                    <input type="text" name="password" id="pw-input"></input>
+                    <input
+                        onChange={e => {
+                            this.setState({ password: e.target.value });
+                        }}
+                        type="password"
+                        name="password"
+                        id="pw-input"
+                    ></input>
                     <label>Password Confirmation</label>
                     <input
-                        type="text"
+                        onChange={e => {
+                            this.setState({ passwordConfirm: e.target.value });
+                        }}
+                        type="password"
                         name="password-confirm"
                         id="pw-confirm"
                     ></input>
-                    <input type="submit" value="Submit" />
+                    {!visible && (
+                        <div id="password-mismatch">Passwords don't match</div>
+                    )}
+                    <button
+                        active={(
+                            !visible && this.state.password != ""
+                        ).toString()}
+                        id="signup-button"
+                    >
+                        Sign up
+                    </button>
                 </form>
             </div>
         );
@@ -30,8 +62,7 @@ const mapStateToProps = state => {};
 
 const mapDispatchToProps = dispatch => {
     return {
-        //onGetLogin: () => dispatch(actionCreators.getLogin()),
-        //onLogIn: () => dispatch(actionCreators.logIn()),
+        onSignUp: () => dispatch(actionCreators.signUp()),
     };
 };
 
