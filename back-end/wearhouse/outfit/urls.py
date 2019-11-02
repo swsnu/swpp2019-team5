587a12c6107952f2f1a7c5ee5d9bd26b10c790eb
Kwaktaewon <tykwak94@gmail.com>
2019-11-02 13:22:59 +0900

    add outfit views

--- back-end/wearhouse/outfit/urls.py
+++ back-end/wearhouse/outfit/urls.py
@@ -3,4 +3,8 @@ from outfit import views
 
 urlpatterns = [
     path('token/', views.token, name='token'),
+    path('outfit/', views.outfit, name=''),
+    path('outfit/<int:outfit_id>/', views.getOutfit, name=''),
+    path('outfit/<int:outfit_id/item>/', views.getItemsOfOutfit, name=''),
+    path('outfit/<int:outfit_id/<int:item_id>/', views.getSpecificItemOfOutfit, name=''),
 ]
--- back-end/wearhouse/outfit/views.py
+++ back-end/wearhouse/outfit/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotAllowed, JsonResponse, HttpResponseBadRequest
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.shortcuts import get_object_or_404
+from .models import Outfit
+from django.forms.models import model_to_dict
+from json import JSONDecodeError
+import json
+
 
 # Create your views here.
 @ensure_csrf_cookie
@@ -7,3 +15,32 @@ def token(request):
         return HttpResponse(status=204)
     else:
         return HttpResponseNotAllowed(['GET'])
+
+def outfit(request):
+    if request.method == 'GET':
+        outfits_all_list = [outfit for outfit in Outfit.objects.all().value()]
+        return JsonResponse(outfits_all_list, safe=False)
+    elif request.method == 'POST':
+        try:
+            body = request.body.decode()
+            #TODO : ADD ITEM BY ML API
+            
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        outfit = Outfit()
+        outfit.save()
+        return JsonResponse(model_to_dict(outfit) ,status = 201)
+    else:
+        return HttpResponseNotAllowed(['GET', 'POST'])
+
+def getOutfit(reqeust, outfit_id):
+    if reqeust.method == 'GET':
+        outfit = Outfit.objects.get(pk = outfit_id)
+     
+    return HttpResponse()
+
+def getItemsOfOutfit(request, outfit_id):
+    return HttpResponse()
+
+def getSpecificItemOfOutfit(request, outfit_id, item_id):
+    return HttpResponse()
