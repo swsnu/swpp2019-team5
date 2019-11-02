dafeaffc7aa0b3a0fb72ba4bdab7912026652747
Heather Kim <haesoo1108@gmail.com>
2019-11-03 01:26:37 +0900

    :art: Added path to main, switched to ConnectedRouter

--- front-end/wearhouse/src/App.js
+++ front-end/wearhouse/src/App.js
@@ -8,25 +8,31 @@ import Browse from "./Containers/Browse/Browse";
 import OutfitDetail from "./Containers/OutfitDetail/OutfitDetail";
 import Login from "./Containers/Auth/Login/Login";
 import Signup from "./Containers/Auth/Signup/Signup";
+import LandingPage from "./Containers/LandingPage/LandingPage";
 
 import "./App.scss";
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/browse" exact component={Browse} />
-                <Route
-                    path="/outfitDetail/:id"
-                    exact
-                    component={OutfitDetail}
-                />
-                <Route path="/calendar" exact component={Calendar} />
-                <Route path="/login" exact component={Login} />
-                <Route path="/signup" exact component={Signup} />
-            </Switch>
-        </BrowserRouter>
-    );
+class App extends React.Component {
+    render() {
+        return (
+            <ConnectedRouter history={this.props.history}>
+                <Switch>
+                    <Route path="/browse" exact component={Browse} />
+                    <Route
+                        path="/outfitDetail/:id"
+                        exact
+                        component={OutfitDetail}
+                    />
+                    <Route path="/calendar" exact component={Calendar} />
+                    <Route path="/login" exact component={Login} />
+                    <Route path="/signup" exact component={Signup} />
+
+                    <Route path="/main" exact component={LandingPage} />
+                    <Redirect exact to="/main" />
+                </Switch>
+            </ConnectedRouter>
+        );
+    }
 }
 
 export default App;
