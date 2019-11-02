9ac7b209cbf51cc172553196efbd83420cb9145a
Heather Kim <haesoo1108@gmail.com>
2019-11-03 01:25:44 +0900

    :lipstick: Implement basic Landing Page

--- front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
+++ front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
@@ -1,8 +1,47 @@
 import React, { Component } from "react";
 
+import Header from "../Header/Header";
+
+import image from "./fashion-images.svg";
+import "./LandingPage.scss";
 class LandingPage extends Component {
+    onLogin = () => {
+        this.props.history.push("/login");
+    };
+
+    onClickSignUp = () => {
+        this.props.history.push("/signup");
+    };
+
     render() {
-        return <div className="Main"></div>;
+        return (
+            <div id="Main">
+                <Header />
+                <div className="intro-content">
+                    <div id="intro-text">
+                        Get your wardrobe organized with <span>WearHouse</span>
+                    </div>
+                    <div id="button-container">
+                        <button
+                            id="login-button"
+                            onClick={() => this.onLogin()}
+                        >
+                            Log In
+                        </button>
+                        <button
+                            id="signup-button"
+                            onClick={() => this.onClickSignUp()}
+                        >
+                            Sign Up
+                        </button>
+                    </div>
+                    <img id="intro-image" src={image} />
+                </div>
+                <div className="intro-content" id="content-2"></div>
+                <div className="intro-content" id="content-3"></div>
+                <footer></footer>
+            </div>
+        );
     }
 }
 export default LandingPage;
