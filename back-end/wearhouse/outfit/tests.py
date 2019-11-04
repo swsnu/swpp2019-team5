0797dd919612a2346ed1c6f1bf2bef284eb4c8e8
Kwaktaewon <tykwak94@gmail.com>
2019-11-05 00:58:46 +0900

    remove migrations

--- back-end/wearhouse/outfit/tests.py
+++ back-end/wearhouse/outfit/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
-
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Outfit
+import json
 # Create your tests here.
+
+def get_csrf_token(client):
+    response = client.get('/api/token/')
+    csrftoken = response.cookies['csrftoken'].value
+    return csrftoken
