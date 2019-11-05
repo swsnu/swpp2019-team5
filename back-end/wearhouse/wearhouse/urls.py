046bb138193283ce2b4375d13e0336477c1d1305
Kwaktaewon <tykwak94@gmail.com>
2019-11-06 00:30:47 +0900

    move each apps' urls to api_urls

--- back-end/wearhouse/wearhouse/urls.py
+++ back-end/wearhouse/wearhouse/urls.py
@@ -15,10 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from outfit import  views as outfit_views
+from item import views as item_views
+from tag import views as tag_views
+from user import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('item.urls')),
-    path('api/', include('outfit.urls')),
-    path('api/', include('tag.urls')),
+    path('token/', user_views.token, name='token'),
+    path('api/tag/<int:tag_id>/', tag_views.getTag, name=''),
+    path('api/tag/<int:tag_id>/item/', tag_views.getItemHavingTag, name=''),
+    path('api/outfit/', outfit_views.outfit, name=''),
+    path('api/outfit/<int:outfit_id>/', outfit_views.getOutfit, name=''),
+    path('api/outfit/<int:outfit_id/item>/', outfit_views.getItemsOfOutfit, name=''),
+    path('api/outfit/<int:outfit_id/<int:item_id>/', outfit_views.getSpecificItemOfOutfit, name=''),
+    path('api/item/<int:item_id>/', item_views.getItem, name='a'),
+    path('api/item/<int:item_id>/outfit/', item_views.getOutfitContainedItem, name=''),
+    path('api/item/<int:item_id>/tag/', item_views.getTagsOfItem, name=''),
+    path('api/item/<int:item_id>/<int:tag_id>/', item_views.getSelectedTag, name=''),
 ]
