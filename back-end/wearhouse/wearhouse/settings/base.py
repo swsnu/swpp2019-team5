219e03c245949ff2beb751dc1e9aad7a1e266bb9
Heather Kim <haesoo1108@gmail.com>
2019-11-14 01:39:05 +0900

    Revert "Revert "Merge branch 'master' into hotfix/login-keyboard-input""
    This reverts commit 8f277b9fcba315591e6b67ee6227edad65adfd59.

--- back-end/wearhouse/wearhouse/settings/base.py
+++ back-end/wearhouse/wearhouse/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,15 +76,6 @@ WSGI_APPLICATION = 'wearhouse.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': "./mysql.cnf",
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        }
-    }
-}
 
 
 # Password validation
