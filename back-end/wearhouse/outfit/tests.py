714a63d7b7c229e1b0603000e9ce22b61cede66d
Kwaktaewon <tykwak94@gmail.com>
2019-11-16 11:12:06 +0900

    delete outfit view for new implementation

--- back-end/wearhouse/outfit/tests.py
+++ back-end/wearhouse/outfit/tests.py
@@ -3,8 +3,3 @@ from django.contrib.auth.models import User
 from .models import Outfit
 import json
 # Create your tests here.
-
-def get_csrf_token(client):
-    response = client.get('/api/token/')
-    csrftoken = response.cookies['csrftoken'].value
-    return csrftoken
