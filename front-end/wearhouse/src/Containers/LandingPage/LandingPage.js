6aadc5f882b4d60941d0eaf4c321245eea4001da
Heather Kim <haesoo1108@gmail.com>
2019-11-20 21:30:55 +0900

    :lipstick: Added footer and section headers to landing page

--- front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
+++ front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 
 import Header from "../Header/Header";
+import logo from "../../Components/HomeButton/Logo_point.png";
 
+import * as actionCreators from "../../store/actions/index";
 import image from "./fashion-images.svg";
 import "./LandingPage.scss";
 class LandingPage extends Component {
+    componentDidMount = () => {
+        this.props.getLogin();
+        if (this.props.isLoggedIn) {
+            this.props.history.push("/browse");
+        }
+    };
+
     onLogin = () => {
         this.props.history.push("/login");
     };
@@ -41,11 +51,52 @@ class LandingPage extends Component {
                         alt="Fashionable People in a list"
                     />
                 </div>
-                <div className="intro-content" id="content-2"></div>
-                <div className="intro-content" id="content-3"></div>
-                <footer></footer>
+                <div className="intro-content" id="content-2">
+                    <h1>What we provide</h1>
+                </div>
+                <div className="intro-content" id="content-3">
+                    <h1>How WearHouse works</h1>
+                </div>
+                <footer>
+                    <div id="footericon"></div>
+                    <div id="footertext">
+                        &copy; SWPP Team WearHouse, 2019
+                        <br />
+                        <br />
+                        Image Recognition API credit:{" "}
+                        <a
+                            href="https://algorithmia.com/algorithms/algorithmiahq/DeepFashion"
+                            target="blank"
+                        >
+                            DeepFashion
+                        </a>
+                        <br />
+                        Weather API credit:{" "}
+                        <a href="https://darksky.net/" target="blank">
+                            DarkSky
+                        </a>
+                    </div>
+                </footer>
             </div>
         );
     }
 }
-export default LandingPage;
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.login.isLoggedIn,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getLogin: () => {
+            dispatch(actionCreators.getLogin());
+        },
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(LandingPage);
