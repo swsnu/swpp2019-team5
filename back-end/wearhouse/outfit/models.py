738e0f13ee85cf0731d1e05db0dd418095076ed3
ttoru96 <ttoru96jk@gmail.com>
2019-11-25 14:19:12 +0900

    Fix errors with models

--- back-end/wearhouse/outfit/models.py
+++ back-end/wearhouse/outfit/models.py
@@ -11,7 +11,7 @@ class Outfit(models.Model):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='outfits_owned_by_user')
     items = models.ManyToManyField(Item, related_name='item_list')
-    image_link = models.CharField(max_length=200)
+    image_link = models.CharField(max_length=100)
     date = models.DateField()
     tempAvg = models.IntegerField()
     tempIcon = models.CharField(max_length=15)
--- back-end/wearhouse/tag/models.py
+++ back-end/wearhouse/tag/models.py
@@ -1,11 +1,8 @@
 from django.db import models
 from user.models import User
-from tag.models import Tag
-# Create your models here.
 
 
-class Item(models.Model):
-    tags = models.ManyToManyField(Tag, related_name='items_with_this_tag')
-    category = models.CharField(max_length=15)
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='items_owned_by_user')
+class Tag(models.Model):
+    name = models.CharField(max_length=20)
+    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name='tags_owned_by_user')
+
