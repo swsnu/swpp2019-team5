10c61e98090ec22fde7fd59e9fb85b0b6158cd81
sungyeon <sungyeonjoo@naver.com>
2019-11-27 14:31:13 +0900

    Resolved travis ci errors - unused variable in Item.js

--- front-end/wearhouse/src/Components/Item/Item.js
+++ front-end/wearhouse/src/Components/Item/Item.js
@@ -112,7 +112,8 @@ class Item extends Component {
         });
     }
     handleAutoComplete = e => {
-        //let option_list = this.state.tags.concat(e.target.value);
+        let option_list = this.state.tags.concat(e.target.value);
+        console.log(option_list);
         //should implement autocomplete feature (from TaeWon's work)
         //autocomplete candidates should be set in option list
     };
