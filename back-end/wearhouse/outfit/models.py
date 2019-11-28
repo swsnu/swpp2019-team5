dc22be2265f7a3d4d2d7e9529accd121662d8ad1
sungyeon <sungyeonjoo@naver.com>
2019-11-28 22:12:56 +0900

    Changed related name

--- back-end/wearhouse/outfit/models.py
+++ back-end/wearhouse/outfit/models.py
@@ -10,10 +10,10 @@ class Outfit(models.Model):
     objects = models.Manager()
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='outfits_owned_by_user')
-    items = models.ManyToManyField(Item, related_name='item_list')
+    items = models.ManyToManyField(
+        Item, related_name='outfits_having_this_item')
     image_link = models.CharField(max_length=100)
     date = models.DateField()
     tempAvg = models.IntegerField(default=100)
     tempIcon = models.CharField(max_length=15, default='')
     satisfaction = models.IntegerField(null=True, blank=True)
-
