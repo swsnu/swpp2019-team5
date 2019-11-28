b006043966743b61f9deb320e8a8c0a23cdfdcff
sungyeon <sungyeonjoo@naver.com>
2019-11-28 22:13:23 +0900

    Changed view name

--- back-end/wearhouse/outfit/urls.py
+++ back-end/wearhouse/outfit/urls.py
@@ -4,7 +4,8 @@ from outfit import views
 urlpatterns = [
     path('token/', views.token, name='token'),
     path('', views.outfit, name=''),
-    path('<int:outfit_id>/', views.getOutfit, name=''),
+    path('<int:outfit_id>/', views.specificOutfit, name=''),
     path('<int:outfit_id/item>/', views.getItemsOfOutfit, name=''),
-    path('<int:outfit_id/<int:item_id>/', views.getSpecificItemOfOutfit, name=''),
+    path('<int:outfit_id/<int:item_id>/',
+         views.getSpecificItemOfOutfit, name=''),
 ]
