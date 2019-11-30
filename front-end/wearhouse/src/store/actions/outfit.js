af28bbfa09154a1aecba8e528109ef90378c7965
sungyeon <sungyeonjoo@naver.com>
2019-11-30 17:41:59 +0900

    Erased hard coding number 11

--- front-end/wearhouse/src/store/actions/outfit.js
+++ front-end/wearhouse/src/store/actions/outfit.js
@@ -83,7 +83,7 @@ export const editOutfit_ = outfit => {
 export const editOutfit = outfit => {
     return dispatch => {
         return axios
-            .put("/api/outfit/" + "11/", outfit)
+            .put("/api/outfit/" + outfit.id, outfit)
             .then(() => {
                 dispatch(editOutfit_(outfit));
             })
