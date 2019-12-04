5d4a4c8b82d96f874a04b46e3fa5c91d53d27fc7
Ubuntu <wearhouse@wearhouse.p1iom5inieietkmgfx0eeuk2qh.psx.internal.cloudapp.net>
2019-12-04 08:00:57 +0000

    add settings for deploy

--- back-end/wearhouse/wearhouse/settings/base.py
+++ back-end/wearhouse/wearhouse/settings/base.py
@@ -26,7 +26,7 @@ SECRET_KEY = '!ua1g&y9ab4md4*s3r2^(1u7uem$xrj)dn&i0g8q4a7q)9!k$='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
