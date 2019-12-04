dbd37dfb8c19b7dc3d512b4c57ccaaf25ed6bdb4
Heather Kim <haesoo1108@gmail.com>
2019-12-04 20:23:54 +0900

    :fire: Remove unintentional changes to backend

--- back-end/wearhouse/item/admin.py
+++ back-end/wearhouse/item/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Item
 
 # Register your models here.
-admin.site.register(Item)
--- back-end/wearhouse/tag/admin.py
+++ back-end/wearhouse/tag/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Tag
 
 # Register your models here.
-admin.site.register(Tag)
