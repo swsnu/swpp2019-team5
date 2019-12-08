2c2edd92c10879e070a3d09e7ab1f2d352965188
Heather Kim <haesoo1108@gmail.com>
2019-12-08 21:53:42 +0900

    :bug: Fix reverse order of uploaded items

--- front-end/wearhouse/src/Containers/Browse/Browse.js
+++ front-end/wearhouse/src/Containers/Browse/Browse.js
@@ -168,7 +168,7 @@ class Browse extends React.Component {
     render() {
         let container = null;
 
-        const outfits = this.props.outfits.map(outfit => {
+        const outfits = this.props.outfits.reverse().map(outfit => {
             return (
                 <Outfit
                     key={outfit.id}
