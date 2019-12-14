c9f998dd944deab4ef0ed2cdcd50573609428457
Heather Kim <haesoo1108@gmail.com>
2019-12-14 18:20:06 +0900

    :bug: Fix iframe properties to fit jsx

--- front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
+++ front-end/wearhouse/src/Containers/LandingPage/LandingPage.js
@@ -71,9 +71,10 @@ class LandingPage extends Component {
                         // width="560"
                         // height="315"
                         src="https://www.youtube.com/embed/4IT6CQgjgUo"
-                        frameborder="0"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowfullscreen
+                        allowFullScreen
+                        title="demo-video"
                     ></iframe>
                 </div>
                 <footer>
