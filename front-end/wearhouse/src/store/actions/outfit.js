bbbbabce1a45983263bd6bfac4cd85c2fae17b1e
Heather Kim <haesoo1108@gmail.com>
2019-12-16 01:01:47 +0900

    :sparkles: Return reverse order from database

--- front-end/wearhouse/src/store/actions/outfit.js
+++ front-end/wearhouse/src/store/actions/outfit.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { push } from "connected-react-router";
 
 export const getOutfits_ = outfits => {
-    return { type: actionTypes.GET_OUTFITS, outfits: outfits };
+    return { type: actionTypes.GET_OUTFITS, outfits: outfits.reverse() };
 };
 
 export const getOutfits = () => {
