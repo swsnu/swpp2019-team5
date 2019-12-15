3ed35fb147ab177da34846a5cc1befaf75de9f9b
Kwaktaewon <tykwak94@gmail.com>
2019-12-15 20:19:01 +0900

    fix models length

--- back-end/wearhouse/outfit/models.py
+++ back-end/wearhouse/outfit/models.py
@@ -12,9 +12,9 @@ class Outfit(models.Model):
         User, on_delete=models.CASCADE, related_name='outfits_owned_by_user')
     items = models.ManyToManyField(
         Item, related_name='outfits_having_this_item')
-    image_link = models.CharField(max_length=100)
+    image_link = models.CharField(max_length=255)
     date = models.DateField(null=True)
     dateWithTime = models.CharField(max_length=255, default='', null=True)
     tempAvg = models.IntegerField(default=100, null=True)
-    tempIcon = models.CharField(max_length=15, default='')
+    tempIcon = models.CharField(max_length=255, default='')
     satisfaction = models.IntegerField(null=True, blank=True)
